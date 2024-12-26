6091eac93f7a41589365fda1315d706698822b36
Wesley00s <wesley300rodrigues@gmail.com>
2024-12-26 09:49:52 -0300

    refactor: remove methods in companion object

--- app/src/main/java/com/example/cine/session/data/remote/datasource/KtorMovieRemoteDatasource.kt
+++ app/src/main/java/com/example/cine/session/data/remote/datasource/KtorMovieRemoteDatasource.kt
@@ -9,6 +9,7 @@ import com.example.cine.session.data.model.SeasonInfo
 import com.example.cine.session.data.remote.MovieRemoteDataSource
 import com.example.cine.session.data.remote.response.movie.ListMoviesResponse
 import com.example.cine.session.data.remote.response.series.ListSeriesResponse
+import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.request.header
@@ -17,144 +18,17 @@ import io.ktor.client.request.setBody
 import javax.inject.Inject
 
 class KtorMovieRemoteDatasource @Inject constructor(
-    private val httpClient: KtorHttpClient
+    private val httpClient: HttpClient
 ) : MovieRemoteDataSource {
     companion object {
         private val httpClient: KtorHttpClient = KtorHttpClient
         const val BASE_URL = BuildConfig.BASE_URL
         const val TOKEN_API = BuildConfig.API_KEY
         const val ACCOUNT_ID = BuildConfig.ACCOUNT_ID
-
-        suspend fun getDetailsMoviesDataResponse(id: Int): Result<MovieInfo> = try {
-            val response = httpClient.httpClientAndroid
-                .get("$BASE_URL/movie/$id?language=en-US") {
-                    header("Authorization", "Bearer $TOKEN_API")
-                    header("accept", "application/json")
-                }
-                .body<MovieInfo>()
-
-            Result.success(response)
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-
-        suspend fun getPopularMovies(page: Int): Result<ListMoviesResponse> = try {
-            val response = httpClient.httpClientAndroid
-                .get("$BASE_URL/movie/popular?language=en-US&page=$page") {
-                    header("Authorization", "Bearer $TOKEN_API")
-                    header("accept", "application/json")
-                }
-                .body<ListMoviesResponse>()
-            Result.success(response)
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-
-        suspend fun getTopRatedMovies(page: Int): Result<ListMoviesResponse> = try {
-            val response = httpClient.httpClientAndroid
-                .get("$BASE_URL/movie/top_rated?language=en-US&page=$page") {
-                    header("Authorization", "Bearer $TOKEN_API")
-                    header("accept", "application/json")
-                }
-                .body<ListMoviesResponse>()
-            Result.success(response)
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-
-        suspend fun getTopRatedSeries(page: Int): Result<ListSeriesResponse> = try {
-            val response = httpClient.httpClientAndroid
-                .get("$BASE_URL/tv/top_rated?language=en-US&page=$page") {
-                    header("Authorization", "Bearer $TOKEN_API")
-                    header("accept", "application/json")
-                }
-                .body<ListSeriesResponse>()
-            Result.success(response)
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-
-        suspend fun getDetailsSeason(id: Int, seasonNumber: Int): Result<SeasonInfo> =
-            try {
-                val response = httpClient.httpClientAndroid
-                    .get("$BASE_URL/tv/$id/season/$seasonNumber?language=en-US") {
-                        header("Authorization", "Bearer $TOKEN_API")
-                        header("accept", "application/json")
-                    }
-                    .body<SeasonInfo>()
-                Result.success(response)
-            } catch (e: Exception) {
-                Result.failure(e)
-            }
-
-
-        suspend fun getFavoritesMovies(
-            page: Int,
-            sessionId: String
-        ): Result<ListMoviesResponse> = try {
-            val response = httpClient.httpClientAndroid
-                .get("$BASE_URL/account/$ACCOUNT_ID/favorite/movies?language=en-US&page=$page&session_id=$sessionId") {
-                    header("Authorization", "Bearer $TOKEN_API")
-                    header("accept", "application/json")
-                }
-                .body<ListMoviesResponse>()
-            Result.success(response)
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-
-
-        suspend fun postFavoriteMovie(
-            sessionId: String,
-            favoriteRequest: FavoriteRequest
-        ): Result<FavoriteResponse> = try {
-            val response = httpClient.httpClientAndroid
-                .post("$BASE_URL/account/$ACCOUNT_ID/favorite?session_id=$sessionId") {
-                    header("Authorization", "Bearer $TOKEN_API")
-                    header("accept", "application/json")
-                    header("Content-Type", "application/json")
-                    setBody(favoriteRequest)
-                }
-                .body<FavoriteResponse>()
-            Result.success(response)
-
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-
-
-        suspend fun postWatchlistMovie(
-            sessionId: String,
-            favoriteRequest: FavoriteRequest
-        ): Result<FavoriteResponse> = try {
-            val response = httpClient.httpClientAndroid
-                .post("$BASE_URL/account/$ACCOUNT_ID/watchlist?session_id=$sessionId") {
-                    header("Authorization", "Bearer $TOKEN_API")
-                    header("accept", "application/json")
-                    header("Content-Type", "application/json")
-                    setBody(favoriteRequest)
-                }
-                .body<FavoriteResponse>()
-            Result.success(response)
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-
-        suspend fun searchMovies(query: String, page: Int): Result<ListMoviesResponse> = try {
-            val response = httpClient.httpClientAndroid
-                .get("$BASE_URL/search/movie?query=$query&page=$page&include_adult=false&language=en-US") {
-                    header("Authorization", "Bearer $TOKEN_API")
-                    header("accept", "application/json")
-                }
-                .body<ListMoviesResponse>()
-            Result.success(response)
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
     }
 
     override suspend fun getDetailsMovies(id: Int): Result<MovieInfo> = try {
-        val response = httpClient.httpClientAndroid
+        val response = httpClient
             .get("$BASE_URL/movie/$id?language=en-US") {
                 header("Authorization", "Bearer $TOKEN_API")
                 header("accept", "application/json")
@@ -166,7 +40,7 @@ class KtorMovieRemoteDatasource @Inject constructor(
     }
 
     override suspend fun getPopularMovies(page: Int): Result<ListMoviesResponse> = try {
-        val response = httpClient.httpClientAndroid
+        val response = httpClient
             .get("$BASE_URL/movie/popular?language=en-US&page=$page") {
                 header("Authorization", "Bearer $TOKEN_API")
                 header("accept", "application/json")
@@ -179,7 +53,7 @@ class KtorMovieRemoteDatasource @Inject constructor(
 
     override suspend fun getTopRatedMovies(page: Int): Result<ListMoviesResponse> =
         try {
-            val response = httpClient.httpClientAndroid
+            val response = httpClient
                 .get("$BASE_URL/movie/top_rated?language=en-US&page=$page") {
                     header("Authorization", "Bearer $TOKEN_API")
                     header("accept", "application/json")
@@ -194,7 +68,7 @@ class KtorMovieRemoteDatasource @Inject constructor(
         page: Int,
         sessionId: String
     ): Result<ListMoviesResponse> = try {
-        val response = httpClient.httpClientAndroid
+        val response = httpClient
             .get("$BASE_URL/account/$ACCOUNT_ID/favorite/movies?language=en-US&page=$page&session_id=$sessionId") {
                 header("Authorization", "Bearer $TOKEN_API")
                 header("accept", "application/json")
@@ -209,7 +83,7 @@ class KtorMovieRemoteDatasource @Inject constructor(
         sessionId: String,
         favoriteRequest: FavoriteRequest
     ): Result<FavoriteResponse> = try {
-        val response = httpClient.httpClientAndroid
+        val response = httpClient
             .post("$BASE_URL/account/$ACCOUNT_ID/favorite?session_id=$sessionId") {
                 header("Authorization", "Bearer $TOKEN_API")
                 header("accept", "application/json")
@@ -227,7 +101,7 @@ class KtorMovieRemoteDatasource @Inject constructor(
         sessionId: String,
         favoriteRequest: FavoriteRequest
     ): Result<FavoriteResponse> = try {
-        val response = httpClient.httpClientAndroid
+        val response = httpClient
             .post("$BASE_URL/account/$ACCOUNT_ID/watchlist?session_id=$sessionId") {
                 header("Authorization", "Bearer $TOKEN_API")
                 header("accept", "application/json")
@@ -241,8 +115,20 @@ class KtorMovieRemoteDatasource @Inject constructor(
     }
 
     override suspend fun searchMovies(query: String, page: Int): Result<ListMoviesResponse> = try {
-        val response = httpClient.httpClientAndroid
+        val response = httpClient
             .get("$BASE_URL/search/movie?query=$query&page=$page&include_adult=false&language=en-US") {
+                header("Authorization", "Bearer $TOKEN_API")
+                header("accept", "application/json")
+            }
+            .body<ListMoviesResponse>()
+        Result.success(response)
+    } catch (e: Exception) {
+        Result.failure(e)
+    }
+
+    override suspend fun getSimilarMovies(id: Int, page: Int): Result<ListMoviesResponse> = try {
+        val response = httpClient
+            .get("$BASE_URL/movie/$id/similar?language=en-US&page=$page") {
                 header("Authorization", "Bearer $TOKEN_API")
                 header("accept", "application/json")
             }
--- app/src/main/java/com/example/cine/session/data/remote/datasource/KtorSerieRemoteDatasource.kt
+++ app/src/main/java/com/example/cine/session/data/remote/datasource/KtorSerieRemoteDatasource.kt
@@ -9,6 +9,7 @@ import com.example.cine.session.data.model.SeasonInfo
 import com.example.cine.session.data.model.SerieInfo
 import com.example.cine.session.data.remote.SerieRemoteDataSource
 import com.example.cine.session.data.remote.response.series.ListSeriesResponse
+import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.request.header
@@ -17,125 +18,16 @@ import io.ktor.client.request.setBody
 import javax.inject.Inject
 
 class KtorSerieRemoteDatasource @Inject constructor(
-    private val httpClient: KtorHttpClient
+    private val httpClient: HttpClient
 ) : SerieRemoteDataSource {
     companion object {
-        private val httpClient: KtorHttpClient = KtorHttpClient
         const val BASE_URL = BuildConfig.BASE_URL
         const val TOKEN_API = BuildConfig.API_KEY
         const val ACCOUNT_ID = BuildConfig.ACCOUNT_ID
-
-        suspend fun getTopRatedSeries(page: Int): Result<ListSeriesResponse> = try {
-            val response = httpClient.httpClientAndroid
-                .get("$BASE_URL/tv/top_rated?language=en-US&page=$page") {
-                    header("Authorization", "Bearer $TOKEN_API")
-                    header("accept", "application/json")
-                }
-                .body<ListSeriesResponse>()
-            Result.success(response)
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-
-        suspend fun getDetailsSeason(id: Int, seasonNumber: Int): Result<SeasonInfo> =
-            try {
-                val response = httpClient.httpClientAndroid
-                    .get("$BASE_URL/tv/$id/season/$seasonNumber?language=en-US") {
-                        header("Authorization", "Bearer $TOKEN_API")
-                        header("accept", "application/json")
-                    }
-                    .body<SeasonInfo>()
-                Result.success(response)
-            } catch (e: Exception) {
-                Result.failure(e)
-            }
-
-        suspend fun getSeasonEpisodes(
-            id: Int,
-            seasonNumber: Int,
-            episodeNumber: Int
-        ): Result<EpisodeInfo> = try {
-            val response = httpClient.httpClientAndroid
-                .get("$BASE_URL/tv/$id/season/$seasonNumber/episode/$episodeNumber?language=en-US") {
-                    header("Authorization", "Bearer $TOKEN_API")
-                    header("accept", "application/json")
-                }
-                .body<EpisodeInfo>()
-            Result.success(response)
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-
-        suspend fun getFavoriteSeries(
-            page: Int,
-            sessionId: String
-        ): Result<ListSeriesResponse> = try {
-            val response = httpClient.httpClientAndroid
-                .get("$BASE_URL/account/$ACCOUNT_ID/favorite/tv?language=en-US&page=$page&session_id=$sessionId") {
-                    header("Authorization", "Bearer $TOKEN_API")
-                    header("accept", "application/json")
-                }
-                .body<ListSeriesResponse>()
-            Result.success(response)
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-
-        suspend fun postFavoriteMovie(
-            sessionId: String,
-            favoriteRequest: FavoriteRequest
-        ): Result<FavoriteResponse> = try {
-            val response = httpClient.httpClientAndroid
-                .post("$BASE_URL/account/$ACCOUNT_ID/favorite?session_id=$sessionId") {
-                    header("Authorization", "Bearer $TOKEN_API")
-                    header("accept", "application/json")
-                    header("Content-Type", "application/json")
-                    setBody(favoriteRequest)
-                }
-                .body<FavoriteResponse>()
-            Result.success(response)
-
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-
-        suspend fun postFavoriteSerie(
-            sessionId: String,
-            favoriteRequest: FavoriteRequest
-        ): Result<FavoriteResponse> = try {
-            val response = httpClient.httpClientAndroid
-                .post("$BASE_URL/account/$ACCOUNT_ID/favorite?session_id=$sessionId") {
-                    header("Authorization", "Bearer $TOKEN_API")
-                    header("accept", "application/json")
-                    header("Content-Type", "application/json")
-                    setBody(favoriteRequest)
-                }
-                .body<FavoriteResponse>()
-            Result.success(response)
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-
-        suspend fun postWatchlistMovie(
-            sessionId: String,
-            favoriteRequest: FavoriteRequest
-        ): Result<FavoriteResponse> = try {
-            val response = httpClient.httpClientAndroid
-                .post("$BASE_URL/account/$ACCOUNT_ID/watchlist?session_id=$sessionId") {
-                    header("Authorization", "Bearer $TOKEN_API")
-                    header("accept", "application/json")
-                    header("Content-Type", "application/json")
-                    setBody(favoriteRequest)
-                }
-                .body<FavoriteResponse>()
-            Result.success(response)
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
     }
 
     override suspend fun getDetailsSeries(id: Int): Result<SerieInfo> = try {
-        val response = httpClient.httpClientAndroid
+        val response = httpClient
             .get("$BASE_URL/tv/$id?language=en-US") {
                 header("Authorization", "Bearer $TOKEN_API")
                 header("accept", "application/json")
@@ -148,7 +40,7 @@ class KtorSerieRemoteDatasource @Inject constructor(
     }
 
     override suspend fun getPopularSeries(page: Int): Result<ListSeriesResponse> = try {
-        val response = httpClient.httpClientAndroid
+        val response = httpClient
             .get("$BASE_URL/tv/popular?language=en-US&page=$page") {
                 header("Authorization", "Bearer $TOKEN_API")
                 header("accept", "application/json")
@@ -162,7 +54,7 @@ class KtorSerieRemoteDatasource @Inject constructor(
 
     override suspend fun getTopRatedSeries(page: Int): Result<ListSeriesResponse> =
         try {
-            val response = httpClient.httpClientAndroid
+            val response = httpClient
                 .get("$BASE_URL/tv/top_rated?language=en-US&page=$page") {
                     header("Authorization", "Bearer $TOKEN_API")
                     header("accept", "application/json")
@@ -177,7 +69,7 @@ class KtorSerieRemoteDatasource @Inject constructor(
         id: Int,
         seasonNumber: Int
     ): Result<SeasonInfo> = try {
-        val response = httpClient.httpClientAndroid
+        val response = httpClient
             .get("$BASE_URL/tv/$id/season/$seasonNumber?language=en-US") {
                 header("Authorization", "Bearer $TOKEN_API")
                 header("accept", "application/json")
@@ -193,7 +85,7 @@ class KtorSerieRemoteDatasource @Inject constructor(
         seasonNumber: Int,
         episodeNumber: Int
     ): Result<EpisodeInfo> = try {
-        val response = httpClient.httpClientAndroid
+        val response = httpClient
             .get("$BASE_URL/tv/$id/season/$seasonNumber/episode/$episodeNumber?language=en-US") {
                 header("Authorization", "Bearer $TOKEN_API")
                 header("accept", "application/json")
@@ -208,7 +100,7 @@ class KtorSerieRemoteDatasource @Inject constructor(
         page: Int,
         sessionId: String
     ): Result<ListSeriesResponse> = try {
-        val response = httpClient.httpClientAndroid
+        val response = httpClient
             .get("$BASE_URL/account/$ACCOUNT_ID/favorite/movies?language=en-US&page=$page&session_id=$sessionId") {
                 header("Authorization", "Bearer $TOKEN_API")
                 header("accept", "application/json")
@@ -223,7 +115,7 @@ class KtorSerieRemoteDatasource @Inject constructor(
         sessionId: String,
         favoriteRequest: FavoriteRequest
     ): Result<FavoriteResponse> = try {
-        val response = httpClient.httpClientAndroid
+        val response = httpClient
             .post("$BASE_URL/account/$ACCOUNT_ID/favorite?session_id=$sessionId") {
                 header("Authorization", "Bearer $TOKEN_API")
                 header("accept", "application/json")
@@ -240,7 +132,7 @@ class KtorSerieRemoteDatasource @Inject constructor(
         sessionId: String,
         favoriteRequest: FavoriteRequest
     ): Result<FavoriteResponse> = try {
-        val response = httpClient.httpClientAndroid
+        val response = httpClient
             .post("$BASE_URL/account/$ACCOUNT_ID/watchlist?session_id=$sessionId") {
                 header("Authorization", "Bearer $TOKEN_API")
                 header("accept", "application/json")
@@ -254,7 +146,7 @@ class KtorSerieRemoteDatasource @Inject constructor(
     }
 
     override suspend fun searchSeries(query: String, page: Int): Result<ListSeriesResponse> = try {
-        val response = httpClient.httpClientAndroid
+        val response = httpClient
             .get("$BASE_URL/search/tv?query=$query&page=$page&include_adult=false&language=en-US") {
                 header("Authorization", "Bearer $TOKEN_API")
                 header("accept", "application/json")
