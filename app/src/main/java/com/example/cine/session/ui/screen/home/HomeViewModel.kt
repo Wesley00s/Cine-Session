f1330749ae7edd271369675cab0e0fb636bf80a6
Wesley00s <wesley300rodrigues@gmail.com>
2024-12-28 10:59:11 -0300

    update: add serie repository in home view model

--- app/src/main/java/com/example/cine/session/ui/screen/home/HomeViewModel.kt
+++ app/src/main/java/com/example/cine/session/ui/screen/home/HomeViewModel.kt
@@ -1,9 +1,9 @@
 package com.example.cine.session.ui.screen.home
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.cine.session.data.repository.MovieRepository
+import com.example.cine.session.data.repository.SeriesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -14,7 +14,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val movieRepository: MovieRepository
+    private val movieRepository: MovieRepository,
+    private val seriesRepository: SeriesRepository
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(HomeUiState())
     val uiState: StateFlow<HomeUiState> = _uiState.asStateFlow()
@@ -22,6 +23,13 @@ class HomeViewModel @Inject constructor(
     fun onEvent(event: HomeUiEvent) {
         when (event) {
             is HomeUiEvent.LoadPupularMovieList -> loadPopularMovieList()
+            is HomeUiEvent.LoadTopRatedMovieList -> loadTopRatedMovieList()
+            is HomeUiEvent.LoadFavoritesMovieList -> loadFavoritesMovieList(
+                event.page,
+                event.sessionId
+            )
+
+            is HomeUiEvent.LoadPopularSeriesList -> loadPopularSeriesList()
         }
     }
 
@@ -29,12 +37,39 @@ class HomeViewModel @Inject constructor(
         viewModelScope.launch {
             _uiState.update { currentUiState ->
                 currentUiState.copy(
-                    movies = movieRepository.getPopularMovies(1)
+                    popularMovies = movieRepository.getPopularMovies(1)
                 )
             }
         }
-
-        Log.d("PopularMovies_ViewModel", "Response:" + _uiState.value.movies.toString())
     }
 
+    private fun loadTopRatedMovieList() {
+        viewModelScope.launch {
+            _uiState.update { currentUiState ->
+                currentUiState.copy(
+                    topRatedMovies = movieRepository.getTopRatedMovies(1)
+                )
+            }
+        }
+    }
+
+    private fun loadFavoritesMovieList(page: Int = 1, sessionId: String = "7db984741d1d3e7d21ef85e902cc935cc6a71887") {
+        viewModelScope.launch {
+            _uiState.update { currentUiState ->
+                currentUiState.copy(
+                    favoritesMovies = movieRepository.getFavoritesMovies(page, sessionId)
+                )
+            }
+        }
+    }
+
+    private fun loadPopularSeriesList() {
+        viewModelScope.launch {
+            _uiState.update { currentUiState ->
+                currentUiState.copy(
+                    popularSeries = seriesRepository.getPopularSeries(1)
+                )
+            }
+        }
+    }
 }
