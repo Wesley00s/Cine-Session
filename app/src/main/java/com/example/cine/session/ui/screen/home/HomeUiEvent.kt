f90bc4471fa521682862c06c167dc8b28ececbdf
Wesley00s <wesley300rodrigues@gmail.com>
2024-12-28 10:58:27 -0300

    update: add popular movies in home screen

--- app/src/main/java/com/example/cine/session/ui/screen/home/HomeUiEvent.kt
+++ app/src/main/java/com/example/cine/session/ui/screen/home/HomeUiEvent.kt
@@ -2,4 +2,7 @@ package com.example.cine.session.ui.screen.home
 
 sealed class HomeUiEvent {
     data class LoadPupularMovieList(val page: Int) : HomeUiEvent()
+    data class LoadTopRatedMovieList(val page: Int) : HomeUiEvent()
+    data class LoadFavoritesMovieList(val page: Int, val sessionId: String) : HomeUiEvent()
+    data class LoadPopularSeriesList(val page: Int) : HomeUiEvent()
 }
