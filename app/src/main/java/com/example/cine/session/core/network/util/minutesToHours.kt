facf30b892440e3a6f3917282b5be152a60b6c1e
Wesley00s <wesley300rodrigues@gmail.com>
2025-01-02 13:10:27 -0300

    refactor: update convert minutes to hour method

--- app/src/main/java/com/example/cine/session/core/network/util/minutesToHours.kt
+++ app/src/main/java/com/example/cine/session/core/network/util/minutesToHours.kt
@@ -3,5 +3,6 @@ package com.example.cine.session.core.network.util
 fun minutesToHours(minutes: Int): String {
     val hours = minutes / 60
     val remainingMinutes = minutes % 60
+    if (hours == 0) return "${remainingMinutes}min"
     return "${hours}h ${remainingMinutes}min"
 }
