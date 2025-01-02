1a453828c92270f9fca28e226d39408cc8815c7e
Wesley00s <wesley300rodrigues@gmail.com>
2025-01-02 12:55:40 -0300

    update: add color on base text

--- app/src/main/java/com/example/cine/session/ui/theme/Type.kt
+++ app/src/main/java/com/example/cine/session/ui/theme/Type.kt
@@ -1,8 +1,8 @@
 package com.example.cine.session.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
@@ -10,61 +10,73 @@ val AppTypography = Typography(
     // "Display"
     displayLarge = TextStyle(
         fontWeight = FontWeight.Black,
-        fontSize = 57.sp
+        fontSize = 57.sp,
+        color = Color.LightGray
     ),
     // "Display"
     displayMedium = TextStyle(
         fontWeight = FontWeight.Black,
-        fontSize = 45.sp
+        fontSize = 45.sp,
+        color = Color.LightGray
     ),
     // "Display"
     displaySmall = TextStyle(
         fontWeight = FontWeight.Black,
-        fontSize = 36.sp
+        fontSize = 36.sp,
+        color = Color.LightGray
     ),
     // "Title XI"
     headlineLarge = TextStyle(
         fontWeight = FontWeight.Bold,
-        fontSize = 24.sp
+        fontSize = 24.sp,
+        color = Color.LightGray
     ),
     // "Title Lg"
     headlineMedium = TextStyle(
         fontWeight = FontWeight.Bold,
-        fontSize = 20.sp
+        fontSize = 20.sp,
+        color = Color.LightGray
     ),
     // "Title Md"
     headlineSmall = TextStyle(
         fontWeight = FontWeight.SemiBold,
-        fontSize = 16.sp
+        fontSize = 16.sp,
+        color = Color.LightGray
     ),
     // "Text Sm"
     titleLarge = TextStyle(
         fontWeight = FontWeight.SemiBold,
-        fontSize = 14.sp
+        fontSize = 14.sp,
+        color = Color.LightGray
     ),
     // "Text Md"
     bodyLarge = TextStyle(
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+        fontSize = 16.sp,
+        color = Color.LightGray
     ),
     // "Text Sm"
     bodyMedium = TextStyle(
         fontWeight = FontWeight.Normal,
-        fontSize = 14.sp
+        fontSize = 14.sp,
+        color = Color.LightGray
     ),
     // "Text Xs"
     bodySmall = TextStyle(
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontSize = 12.sp,
+        color = Color.LightGray
     ),
     // "Action"
     labelLarge = TextStyle(
         fontWeight = FontWeight.SemiBold,
-        fontSize = 16.sp
+        fontSize = 16.sp,
+        color = Color.LightGray
     ),
     // "Subtitle"
     labelMedium = TextStyle(
         fontWeight = FontWeight.Medium,
-        fontSize = 14.sp
+        fontSize = 14.sp,
+        color = Color.LightGray
     )
 )
